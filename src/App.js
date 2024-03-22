56ca4a77b486ad645a39e72beeca0f9f6316f7f0
MARUPAKA DHANUSH <dhanushmarupaka075@gmail.com>
2024-03-22 14:49:52 +0530

    8

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useState, useEffect }  from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Select from "react-select"
-import Recipe from './food'
-// import Dropdownbox from './dropdown';
+import Recipe from './components/food';
+import NavBar  from './components/Navbar';
+import Footer from './components/Footer'
+import Pagination from './components/Pagination'
+import FormComponent from './components/FormComponent';
 import './App.css';
 
 
@@ -66,8 +69,6 @@ const App = () => {
   const [cuisine,setcuisine] = useState('')
   const [cuisinety,setcuisinety] = useState('')
 
-  // const expreq = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${app_id}&app_key=${app_key}&cuisineType=Indian&mealType=Lunch&dishType=Main%20course&imageSize=REGULAR`
-  
   useEffect(() => {
     getreciepe()
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -76,8 +77,6 @@ const App = () => {
   const getreciepe = async () =>{
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${submit}&app_id=${app_id}&app_key=${app_key}${deitty}${cuisinety}${mealty}${dishty}`)
     const data = await response.json()
-    // console.log(data.hits[0].recipe.label)
-    // console.log(data.hits[1].recipe.label)
     console.log(data.hits)
     
     setrecipes(data.hits)
@@ -103,7 +102,7 @@ const App = () => {
     if (selectedOption.value === '' ){
       setdiet(' ') 
     }else {
-       setdiet(`&diet=${selectedOption.value}`)
+      setdiet(`&diet=${selectedOption.value}`)
     }
   }
 
@@ -112,7 +111,7 @@ const App = () => {
     if (selectedOption.value === '' ){
       setdish(' ') 
     }else {
-       setdish(`&dishType=${selectedOption.value}`)
+      setdish(`&dishType=${selectedOption.value}`)
     }
   }
 
@@ -121,7 +120,7 @@ const App = () => {
     if (selectedOption.value === '' ){
       setcuisine(' ') 
     }else {
-       setcuisine(`&cuisineType=${selectedOption.value}`)
+      setcuisine(`&cuisineType=${selectedOption.value}`)
     }
   }
 
@@ -135,52 +134,66 @@ const App = () => {
     setsearch('')
   }
 
+ 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recipesPerPage] = useState(10);
+  const [sortBy, setSortBy] = useState('');
+
+  const indexOfLastRecipe = currentPage * recipesPerPage;
+  const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+  const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+
+  const sortRecipes = (option) => {
+    setSortBy(option);
+    let sortedRecipes = [...recipes];
+    if (option === 'name') {
+      sortedRecipes.sort((a, b) => a.recipe.label.localeCompare(b.recipe.label));
+    } else if (option === 'calories') {
+      sortedRecipes.sort((a, b) => a.recipe.calories - b.recipe.calories);
+    }
+    setrecipes(sortedRecipes);
+  };
+
+ 
+  const toggleFavorite = (index) => {
+    const updatedRecipes = [...recipes];
+    updatedRecipes[index].favorite = !updatedRecipes[index].favorite;
+    setrecipes(updatedRecipes);
+  };
+  const [debouncedSearch, setDebouncedSearch] = useState('');
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearch(search);
+    }, 500); 
+    return () => clearTimeout(timer);
+  }, [search]);
+
+  const apiUrl = useMemo(() => {
+    return `https://api.edamam.com/api/recipes/v2?type=public&q=${debouncedSearch}&app_id=${app_id}&app_key=${app_key}${deitty}${cuisinety}${mealty}${dishty}`;
+  }, [debouncedSearch, deitty, cuisinety, mealty, dishty]);
+
+  // Fetch recipes
+  useEffect(() => {
+    const getRecipes = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setrecipes(data.hits);
+      } catch (error) {
+        console.error('Error fetching recipes:', error);
+      }
+    };
+    getRecipes();
+  }, [apiUrl]);
+
   return (
-    <div className="h-s grid place-items-center text-white">
-
-      <div className="upbar" id='upbar'>
-        <h3 className="idfc" id='upbar'>Cook<span className='bg-gradient-to-r from-indigo-500 to-cyan-400 bg-clip-text text-transparent'>Genie</span> - food recipe generator</h3>      
-        <nav>
-            <ul className="nav_bar">
-               <li><a href="#home"><b>Home</b></a></li>
-               <li><a href="#about"><b>About me</b></a></li>
-               <li><a href="#contact"><b>Contact</b></a></li>
-            </ul>
-        </nav>
-      </div>
-
-      <div className='pro'>
-        <h1 className='title'>Eliminate mealtime uncertainties<br />with Cook<span className='bg-gradient-to-r from-indigo-500 to-cyan-400 bg-clip-text text-transparent'>Genie</span></h1>
-      </div>
+    <div className="h-s grid place-items-center bg-container text-white">
+      <NavBar/>
 
       <div className='mainform'>
-      <form  className='my-2rem space-y-[1rem] shadow-md rounded px-8 pt-6 pb-8 mb-4  text-white border-2 border-slate-500 rounded-xl' onSubmit={getsubmit}>
-        <div>
-          <span id='types'>Search : </span><br/><br/><input className='dark:text-black rounded  py-2 px-3' type = "text" placeholder='search' value={search} onChange={research}/>
-        </div>
-
-        <div className='my-1rem' id='options'>
-          <span id='types'>Meal Type : </span><br/><br/><Select className='text-black' options={mealtypeoptions} onChange={mealtype} styles={{width:'36vw'}} />
-        </div>
-
-        <div className='my-1rem' id='options'>
-          <span id='types'>Diet Type :</span><br/><br/><Select className='text-black' options={dietoptions} onChange={diettype}/>
-        </div>
-
-        <div className='my-1rem' id='options'>
-          <span id='types'>Dish Type : </span><br/><br/><Select className='text-black' options={dishtypeoptions} onChange={dishtype}/>
-        </div>
-
-        <div className='my-1rem' id='options'>
-          <span id='types'>Cuisine Type : </span><br/><br/><Select className='text-black' options={cuisinetypeoptions} onChange={cuisinetype}/>
-        </div><br />
-
-        <button className='py-2 px-4 text-lg bg-gradient-to-r from-indigo-500 to-cyan-400 text-white rounded-lg font-medium ml-8 ' type='submit' >
-          <b className='drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]' >Search </b>
-        </button>
-      </form> 
+        <FormComponent mealtypeoptions={mealtypeoptions} dietoptions={dietoptions} dishtypeoptions={dishtypeoptions} cuisinetypeoptions={cuisinetypeoptions}  research={research} mealtype={mealtype} dishtype={dishtype} diettype={diettype} getsubmit={getsubmit} cuisinetype={cuisinetype}  sortBy={sortBy} sortRecipes={sortRecipes} Select={Select}/>
       </div>
-
 
       <div className='output_recipe'>
       {recipes.map(recipe => (
@@ -193,11 +206,25 @@ const App = () => {
         />
       ))}
       </div>
+      <Pagination recipes={recipes} setCurrentPage={setCurrentPage} currentPage={currentPage} recipesPerPage={recipesPerPage}/>
+
+      <div className='output_recipe'>
+        {currentRecipes.map((recipe, index) => (
+          <Recipe
+            key={index}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+            favorite={recipe.favorite || false}
+            toggleFavorite={() => toggleFavorite(index)}
+          />
+        ))}
+      </div>
       
-      {/* <footer>Made with lust by <a href='https://charandevreddy.github.io/portfolio/'>Charan Dev Reddy</a></footer> */}
-      
+    <Footer/>
     </div>
   );
 }
 
-export default App;
+export default App
